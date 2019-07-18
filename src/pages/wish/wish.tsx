a6a3475c38a8f89a53b01d1274be2e08498542d5
harvey <harvey.jiang.cn@gmail.com>
2019-07-18 22:09:34 +0800

    update

---
+++ src/pages/wish/wish.tsx
@@ -0,0 +1,130 @@
+import { ComponentClass } from 'react'
+import Taro, { Component, Config } from '@tarojs/taro'
+import { View, Button, Text } from '@tarojs/components'
+import { connect } from '@tarojs/redux'
+
+import { add, minus, asyncAdd } from '../../actions/counter'
+import { AtList, AtListItem, AtSearchBar, AtPagination } from 'taro-ui'
+import './wish.scss'
+
+// #region 书写注意
+//
+// 目前 typescript 版本还无法在装饰器模式下将 Props 注入到 Taro.Component 中的 props 属性
+// 需要显示声明 connect 的参数类型并通过 interface 的方式指定 Taro.Component 子类的 props
+// 这样才能完成类型检查和 IDE 的自动提示
+// 使用函数模式则无此限制
+// ref: https://github.com/DefinitelyTyped/DefinitelyTyped/issues/20796
+//
+// #endregion
+
+type PageStateProps = {
+  counter: {
+    num: number
+  }
+}
+
+type PageDispatchProps = {
+  add: () => void
+  dec: () => void
+  asyncAdd: () => any
+}
+
+type PageOwnProps = {}
+
+type PageState = {}
+
+type IProps = PageStateProps & PageDispatchProps & PageOwnProps
+
+interface Wish {
+  props: IProps;
+}
+
+@connect(({ counter }) => ({
+  counter
+}), (dispatch) => ({
+  add() {
+    dispatch(add())
+  },
+  dec() {
+    dispatch(minus())
+  },
+  asyncAdd() {
+    dispatch(asyncAdd())
+  }
+}))
+
+
+class Wish extends Component {
+
+  /**
+ * 指定config的类型声明为: Taro.Config
+ *
+ * 由于 typescript 对于 object 类型推导只能推出 Key 的基本类型
+ * 对于像 navigationBarTextStyle: 'black' 这样的推导出的类型是 string
+ * 提示和声明 navigationBarTextStyle: 'black' | 'white' 类型冲突, 需要显示声明类型
+ */
+  config: Config = {
+    navigationBarTitleText: '礼卷列表'
+  }
+
+  constructor() {
+    super(...arguments)
+    this.state = {
+      current: 0
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    console.log(this.props, nextProps)
+  }
+
+  componentWillUnmount() { }
+
+  componentDidShow() { }
+
+  componentDidHide() { }
+
+  handleClick(value) {
+    console.log(value)
+    this.setState({
+      current: value
+    })
+  }
+  render() {
+    return (
+      <View className='index'>
+         <AtList>
+          <AtListItem
+            title='京东-物品-100元'
+            arrow='down'
+            thumb='https://img12.360buyimg.com/jdphoto/s72x72_jfs/t6160/14/2008729947/2754/7d512a86/595c3aeeNa89ddf71.png'
+          />
+          <AtListItem
+            title='苏宁-其他-200元'
+            note='苏宁平台礼品卷,拼单'
+            arrow='down'
+            thumb='http://img10.360buyimg.com/jdphoto/s72x72_jfs/t5872/209/5240187906/2872/8fa98cd/595c3b2aN4155b931.png'
+          />
+          <AtListItem
+            title='票 加油票 500元'
+            note='描述信息'
+            extraText='详情'
+            arrow='down'
+            thumb='http://img12.360buyimg.com/jdphoto/s72x72_jfs/t10660/330/203667368/1672/801735d7/59c85643N31e68303.png'
+          />
+        </AtList>
+        
+
+      </View>
+    )
+  }
+}
+
+// #region 导出注意
+//
+// 经过上面的声明后需要将导出的 Taro.Component 子类修改为子类本身的 props 属性
+// 这样在使用这个子类时 Ts 才不会提示缺少 JSX 类型参数错误
+//
+// #endregion
+
+export default Wish as ComponentClass<PageOwnProps, PageState>
